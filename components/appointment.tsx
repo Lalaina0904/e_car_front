fcd9311ee793e5f4cb04419828744f6c69658b93
Marc Herilala <hei.marc.3@gmail.com>
2024-06-07 22:16:09 +0300

    feat:appointment list

--- components/appointment.tsx
+++ components/appointment.tsx
@@ -9,11 +9,12 @@ type Appointment = {
     contact: string;
     appointmentDate: string;
     idCar: string;
-    idAppointment: string;
+    id: string;
     status: string;
 };
+  
 export const AppointmentList = () => {
-    const dataProvider = useDataProvider();
+ const dataProvider = useDataProvider(); 
     const [appointments, setAppoinments] = useState<Appointment[]>([]);
     useEffect(() => {
         dataProvider
@@ -24,39 +25,68 @@ export const AppointmentList = () => {
             })
             .then(({ data }) => setAppoinments(data));
     }, []);
+  
+   
 
     return (
-        <div>
-            <div className="text-center text-3xl uppercase font-medium py-4">
+      <div>
+         <div className="text-center text-3xl uppercase font-medium py-4">
                 appointments
-            </div>
+         </div>
+         <div>
+                {appointments.map((appointment, index) => (
+                    <Appointment key={index} appointment={appointment} />
+                ))}
+         </div>
+      </div>
+    );
+};
+const Appointment = ({ appointment }: { appointment: Appointment }) => {
+     const dataProvider = useDataProvider(); 
+     const handleStatus = (id: string, status: string) => {
+        dataProvider
+            .update("appointment/update/", {
+                id: {id},
+                data: {status},
+                previousData:{},
+            },
+         
+           
+        )
+            .then(() => {
+                console.log("status updated");
+            });
+    }
+    return(
+      <div>
+           
 
             <div className="flex justify-between items-center bg-white p-5 rounded-xl shadow-xl">
                 <div className="flex flex-col gap-4 ">
                     <div className="flex flex-row gap-1">
                         <div className="font-semibold capitalize">name :</div>
-                        <div>name</div>
+                        <div>{appointment.firstName}</div>
                     </div>
                     <div className="flex flex-row gap-1">
                         <div className="font-semibold capitalize">email : </div>
-                        <div>email@gmail.com</div>
+                        <div>{appointment.email}</div>
                     </div>
                     <div className="flex flex-row gap-1">
                         <div className="font-semibold capitalize">date : </div>
-                        <div>2023-0393773</div>
+                        <div>{appointment.appointmentDate}</div>
                     </div>
                     <div className="flex flex-row gap-1">
                         <div className="font-semibold capitalize">
                             message :
                         </div>
-                        <div>message</div>
+                        <div>{appointment.message}</div>
                     </div>
                 </div>
                 <div className="flex flex-col gap-2">
-                    <Button className="bg-[#013248] uppercase">validé</Button>
-                    <Button className="bg-[#ac1e1e] uppercase">refusé</Button>
+                    <Button className="bg-[#013248] uppercase" onClick={()=>handleStatus(appointment.id,"update")}>validé</Button>
+                    <Button className="bg-[#ac1e1e] uppercase" onClick={()=>handleStatus(appointment.id,"refuse")}>refusé</Button>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
